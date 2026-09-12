81feec70282279f0ed25f0ce19220cec5134be15
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-09-12 14:59:09 +0500

    Add strings.js with string concatenation examples

---
+++ JavaScript/strings.js
@@ -0,0 +1,11 @@
+// Strings
+const name = 'arbish' ;
+const age = 18;
+
+// Concatenation.
+console.log('My name is ' + name + ' and I am ' + age);
+
+// ES6 --> template string
+console.log(`My name is ${name} and I am ${age}`);
+const hello = `My name is ${name} and I am ${age}`;
+console.log(hello);
