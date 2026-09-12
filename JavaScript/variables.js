866a5b801167bcb3eb2daf026cadc246a6cc9132
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-09-12 14:58:11 +0500

    Create variables.js

---
+++ JavaScript/variables.js
@@ -0,0 +1,16 @@
+// Variables.
+// var , let , const
+// var (globally scoped), not used anymore.
+// let , const added with ES6 or ES2015.
+// let and const --> (block level scope).
+// Difference between let and const.
+// with 'let', values can be reassgined.
+let age = 30;
+age = 70;
+console.log(age);
+// whereas, const can't be reassigned.
+// const year = 90;
+// year 89;
+// console.log(year);
+// always use const, wen yk  you wont be reassigning the values.
+// this makes your code more robust, more secure, less prone to errors.
