3e21a048b14423825d8eda8b3574225acb467a92
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-09-12 14:59:28 +0500

    Create stringProperties.js

---
+++ JavaScript/stringProperties.js
@@ -0,0 +1,22 @@
+const s = 'technology, computer, it, code';
+
+// String Properties.
+// Properties doesnt ve parenthesis.
+// methods ve parenthesis.
+
+const length = s.length;
+console.log(length);
+
+const upcase = s.toUpperCase();
+console.log(upcase);
+
+const lowcase = s.toLowerCase();
+console.log(lowcase);
+
+// const substr = s.substring(0,5);
+const substr = s.substring(0,5).toUpperCase();
+console.log(substr);
+
+console.log(s.split(', ')); 
+
+ 
