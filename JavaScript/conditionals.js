8efbeff7ee8f10ad0bdd7eb984487385b576b8dc
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-09-12 15:02:01 +0500

    Create conditionals.js

---
+++ JavaScript/conditionals.js
@@ -0,0 +1,14 @@
+// Conditionals
+
+// if used souble equals, it wont match the datatype, its just
+// the values.
+// const x = 10; // true
+const x = '10'; // true.
+if(x === 10){
+    console.log('x is 10');
+} else {
+    console.log(' x isnt 10');        
+}
+
+// whereas, === (triple equal) also matches the datatype.
+
