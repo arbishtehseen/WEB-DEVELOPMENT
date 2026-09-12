b15e0eb9bf4526fdb31ec1ed2cdc29db4c99e173
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-09-12 15:00:43 +0500

    Create arrayOfObjects.js

---
+++ JavaScript/arrayOfObjects.js
@@ -0,0 +1,21 @@
+// Array of Objects
+const todos = [
+    {
+        id : 1 ,
+        text : 'Take out trash' ,
+        isCompleted : true 
+    },
+    {
+        id : 2 ,
+        text : 'Meeting with boss' ,
+        isCompleted : true 
+    },
+    {
+        id : 3 ,
+        text : 'Dentist Appointment' ,
+        isCompleted : false
+    }
+];
+
+console.log(todos);
+console.log(todos[1].text);
