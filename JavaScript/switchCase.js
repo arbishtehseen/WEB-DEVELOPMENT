7d7827f941eee78c190f2457fd5ffee95bee2a15
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-09-12 15:02:39 +0500

    Create switchCase.js

---
+++ JavaScript/switchCase.js
@@ -0,0 +1,15 @@
+const x = 11;
+
+const color = x > 10? 'red' : 'blue';
+
+switch(color){
+   case 'red': 
+   console.log('color is Red');
+   break;
+   case 'blue' :
+   console.log('color is Blue');
+   break;
+   default :
+   console.log('color isnt Red or Blue');
+   break;
+}
