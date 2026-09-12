f29a004f926bd5ef7cde893c8873f9a3d78ab75f
Arbish Randhawah <arbishtehseen20@gmail.com>
2026-09-12 14:58:44 +0500

    Create dataTypes.js

---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,20 @@
+// Datatypes.
+// primitive datatype --> data is directly assigned to the memory.
+// String, Numbers , Boolean, null , undefined, Symbol(ES6).
+
+const name = 'arbish' ; //  ts string.
+// strings can ve both single and double quotes.
+const age = 30;
+const rating = 4.5; // there is no int or float datatype in js its just numbers
+const isCool = true;
+const x = null; // empty
+const y = undefined;
+let z; // thats gonna be undefined.
+
+console.log(typeof name);  // string
+console.log(typeof age);   // number
+console.log(typeof rating);  // number
+console.log(typeof isCool);  // boolean
+console.log(typeof x);  // object
+console.log(typeof y);  // undefined
+console.log(typeof z);  // undefined
